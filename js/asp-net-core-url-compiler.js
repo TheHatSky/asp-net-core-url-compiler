6b137e9b036780fc8cdc798b85ec388121173540
Artem Sakhatskiy <sakhatskiy@yahoo.com>
2016-12-11 00:31:42 +0300

    Housekeeping

--- js/asp-net-core-url-compiler.js
+++ js/asp-net-core-url-compiler.js
@@ -1,22 +1,39 @@
 "use strict";
-exports.compileUrl = function (urlTemplate, parameters) {
-    if (urlTemplate == null)
-        throw new Error('Can\'t compile null URL.');
-    if (parameters === undefined)
-        throw new Error('Can\'t compile URL with undefined parameters.');
-    if (parameters == null)
-        throw new Error('Can\'t compile URL with null parameters.');
-    for (var field in parameters) {
-        var parameter = parameters[field];
-        switch (typeof parameter) {
-            case 'string':
-            case 'number':
-                break;
-            default:
-                throw new Error("Can't compile URL with complex parameter '" + field + "'.");
+var parameter_1 = require("./parameter");
+var Compiler;
+(function (Compiler) {
+    Compiler.compileUrl = function (urlTemplate, parameterValues) {
+        if (urlTemplate == null)
+            throw new Error('Can\'t compile null URL.');
+        if (parameterValues === undefined)
+            throw new Error('Can\'t compile URL with undefined parameters.');
+        if (parameterValues == null)
+            throw new Error('Can\'t compile URL with null parameters.');
+        var parametersInUrl = parameter_1["default"].parseUrlTemplate(urlTemplate);
+        for (var _i = 0, parametersInUrl_1 = parametersInUrl; _i < parametersInUrl_1.length; _i++) {
+            var parameter = parametersInUrl_1[_i];
+            var value = parameterValues[parameter.name];
+            urlTemplate = parameter.applyToUrlTemplate(urlTemplate, value);
         }
-        var encodedParameter = encodeURIComponent(parameter.toString());
-        urlTemplate = urlTemplate.replace(new RegExp("{" + field + "}", 'g'), encodedParameter);
-    }
-    return urlTemplate;
-};
+        /*
+                for (let field in parameters)
+                {
+                    let parameter = parameters[field];
+                    switch (typeof parameter)
+                    {
+                        case 'string':
+                        case 'number':
+                            break;
+                        default:
+                            throw new Error(`Can't compile URL with complex parameter '${field}'.`);
+                    }
+        
+                    let encodedParameter = encodeURIComponent(parameter.toString());
+        
+                    urlTemplate = urlTemplate.replace(new RegExp(`{${field}}`, 'g'), encodedParameter);
+                }
+        */
+        return urlTemplate;
+    };
+})(Compiler || (Compiler = {}));
+module.exports = Compiler;
